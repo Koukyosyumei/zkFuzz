1fd0727c8d697aca90b219baa7ed02a081fb4a4b
Koukyosyumei <koukyosyumei@hotmail.com>
2024-11-19 10:11:15 -0500

    update

--- tcct/aggregate.sh
+++ tcct/aggregate.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-# 中間ファイルの準備
+# Intermediate Files
 meta_file="meta.csv"
 trace_file="trace.csv"
 side_file="side.csv"
 
-# 各セクションを分離
+# Separate each section
 awk '/template_name/{flag=1; print > "'$meta_file'"; next} /Total_Constraints/{flag++; if(flag==2) print > "'$trace_file'"; else print > "'$side_file'"; next} flag==1 {print > "'$meta_file'"} flag==2 {print > "'$trace_file'"} flag==3 {print > "'$side_file'"}'
 
-# 最終行を除いてファイルを一時ファイルに書き出す
+# Remove the last line
 head -n $((lines - 1)) "$side_file" > "$side_file.tmp"
-# 一時ファイルを元のファイルに移動
 mv "$side_file.tmp" "$side_file"
 
-# 平均値計算関数
 calculate_average() {
   file=$1
-  header=$(head -n 1 "$file") # ヘッダー取得
-  echo "$header"              # ヘッダー出力
+  header=$(head -n 1 "$file")
+  echo "$header"              
   awk -F, '
-  NR==1 { next } # ヘッダーをスキップ
+  NR==1 { next }
   {
     for (i=1; i<=NF; i++) {
       sum[i] += $i;
@@ -36,13 +34,50 @@ calculate_average() {
   }' "$file"
 }
 
-# 集計と結果出力
+calculate_median() {
+  file=$1
+  header=$(head -n 1 "$file")
+  echo "$header"              
+  awk -F, '
+  NR==1 { next }
+  {
+    for (i=1; i<=NF; i++) {
+      data[i][count[i]++] = $i;
+      sum[i] += $i;
+    }
+  }
+  END {
+    for (i=1; i<=NF; i++) {
+      if (count[i] > 0) {
+        # average
+        avg = sum[i] / count[i];
+
+        # median
+        asort(data[i]);
+        if (count[i] % 2 == 1) {
+          median = data[i][int(count[i] / 2) + 1];
+        } else {
+          median = (data[i][int(count[i] / 2)] + data[i][int(count[i] / 2) + 1]) / 2;
+        }
+
+        printf "%s,", median;
+      }
+    }
+    print "";
+  }' "$file"
+}
+
+# Aggregation
 echo "Meta Information Average:"
 calculate_average "$meta_file"
 echo "Trace Constraints Average:"
 calculate_average "$trace_file"
+echo "Trace Constraints Median:"
+calculate_median "$trace_file"
 echo "Side Constraints Average:"
 calculate_average "$side_file"
+echo "Side Constraints Median:"
+calculate_median "$side_file"
 
-# 中間ファイルのクリーンアップ
+# Cleanup
 rm -f $meta_file $trace_file $side_file
