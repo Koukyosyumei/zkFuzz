4c1e47c7bd37f09b09934e1df2a13547e6021b1c
Koukyosyumei <koukyosyumei@hotmail.com>
2024-11-24 09:33:30 -0500

    update

--- tcct/src/main.rs
+++ tcct/src/main.rs
@@ -68,12 +68,17 @@ fn start() -> Result<(), ()> {
                 info!("Final State: {:?}", s);
             }
 
+            println!("================ TCCT Report ================");
+            println!("📊 Execution Summary:");
+            println!("  - Total Paths Explored: {}", sexe.final_states.len());
+
             if user_input.flag_printout_stats {
                 println!("----------------------\n*Stats of Trace Constraint*");
                 print_constraint_summary_statistics_pretty(&sexe.trace_constraint_stats);
                 println!("----------------------\n*Stats of Side Constraint*");
                 print_constraint_summary_statistics_pretty(&sexe.side_constraint_stats);
             }
+            println!("=============================================");
         }
         _ => {
             warn!("Cannot Find Main Call");
