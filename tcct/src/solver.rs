3b5f49e048687735f136db1a45dfacfd063066e3
Koukyosyumei <koukyosyumei@hotmail.com>
2024-12-06 12:48:56 -0500

    update

--- tcct/src/solver.rs
+++ tcct/src/solver.rs
@@ -275,10 +275,12 @@ pub fn brute_force_search(
     );
     io::stdout().flush().unwrap();
 
+    println!("\nSearch completed");
     println!(
-        "\nSearch completed. Total iterations: {}",
+        "  ├─ Total iterations: {}",
         current_iteration.load(Ordering::SeqCst)
     );
+    println!("  └─ Verification result: {}", flag);
 
     if is_vulnerable(&flag) {
         Some(CounterExample {
