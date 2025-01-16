af8eb845b3439471d73f28e2e1e2d4ac59ca3465
Koukyosyumei <koukyosyumei@hotmail.com>
2025-01-16 17:14:40 -0500

    update

--- tcct/tests/sample/test_recursive_call.circom
+++ tcct/tests/sample/test_recursive_call.circom
@@ -7,7 +7,11 @@ template Recursive(len) {
 
     var batch = 3;
     if (len < batch) {
-        out <== inputs[0];
+        var sum = 0;
+        for (var j = 0; j < len; j++) {
+            sum += inputs[j];
+        }
+        out <== sum;
     } else {
         var t[len - batch + 1];
         t[0] = 12;
@@ -18,4 +22,4 @@ template Recursive(len) {
     }
 }
 
-component main = Recursive(7);
+component main = Recursive(8);
--- tcct/tests/symbolic_trace_emulation_test.rs
+++ tcct/tests/symbolic_trace_emulation_test.rs
@@ -7,11 +7,10 @@ use num_bigint_dig::BigInt;
 use num_traits::identities::Zero;
 use num_traits::One;
 
-
 use rustc_hash::FxHashMap;
 use tcct::executor::symbolic_execution::SymbolicExecutor;
 use tcct::executor::symbolic_setting::get_default_setting_for_symbolic_execution;
-use tcct::executor::symbolic_value::{OwnerName, SymbolicName};
+use tcct::executor::symbolic_value::{OwnerName, SymbolicAccess, SymbolicName, SymbolicValue};
 use tcct::solver::utils::emulate_symbolic_trace;
 
 use crate::utils::{execute, prepare_symbolic_library};
@@ -66,4 +65,55 @@ fn test_emulate_if_else() {
         &mut sexe.symbolic_library,
     );
     assert_eq!(assignment[&main_out], BigInt::zero());
+}
+
+#[test]
+fn test_recursive_call() {
+    let path = "./tests/sample/test_recursive_call.circom".to_string();
+    let prime = BigInt::from_str(
+        "21888242871839275222246405745257275088548364400416034343698204186575808495617",
+    )
+    .unwrap();
+
+    let (mut symbolic_library, program_archive) = prepare_symbolic_library(path, prime.clone());
+    let setting = get_default_setting_for_symbolic_execution(prime.clone(), false);
+
+    let mut sexe = SymbolicExecutor::new(&mut symbolic_library, &setting);
+    execute(&mut sexe, &program_archive);
+
+    let mut assignment = FxHashMap::from_iter((0..8).into_iter().map(|i| {
+        (
+            SymbolicName::new(
+                sexe.symbolic_library.name2id["inputs"],
+                Rc::new(vec![OwnerName {
+                    id: sexe.symbolic_library.name2id["main"],
+                    access: None,
+                    counter: 0,
+                }]),
+                Some(vec![SymbolicAccess::ArrayAccess(
+                    SymbolicValue::ConstantInt(BigInt::from(i)),
+                )]),
+            ),
+            BigInt::from(i),
+        )
+    }));
+
+    let _ = emulate_symbolic_trace(
+        &prime,
+        &sexe.cur_state.symbolic_trace,
+        &mut assignment,
+        &mut sexe.symbolic_library,
+    );
+
+    let main_out = SymbolicName::new(
+        sexe.symbolic_library.name2id["out"],
+        Rc::new(vec![OwnerName {
+            id: sexe.symbolic_library.name2id["main"],
+            access: None,
+            counter: 0,
+        }]),
+        None,
+    );
+
+    assert_eq!(assignment[&main_out], BigInt::from(19));
 }
