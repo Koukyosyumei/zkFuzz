adcf7cbafb52ef924bd7d5937616b7bb28e64135
Koukyosyumei <koukyosyumei@hotmail.com>
2025-03-24 15:30:43 -0400

    update

--- src/mutator/mutation_test_update_input_fn.rs
+++ src/mutator/mutation_test_update_input_fn.rs
@@ -88,9 +88,16 @@ pub fn update_input_population_with_fitness_score(
             } else {
                 parent1.clone()
             };
-            for (_, val) in child.iter_mut() {
+            let mut keys: Vec<_> = child.keys().cloned().collect();
+            keys.sort();
+            for k in keys.iter() {
+                //let val = child.get(k).unwrap();
                 if rng.gen::<f64>() < mutation_config.mutation_rate {
-                    *val = draw_bigint_with_probabilities(&mutation_config, rng).unwrap();
+                    //*val = draw_bigint_with_probabilities(&mutation_config, rng).unwrap();
+                    child.insert(
+                        k.clone().clone(),
+                        draw_bigint_with_probabilities(&mutation_config, rng).unwrap(),
+                    );
                 }
             }
             child
